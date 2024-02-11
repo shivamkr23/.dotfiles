7091fb64b5ac957ef39f69aefc19e00243d24e4b
Shivam Kumar <shivamkr.kr23@gmail.com>
2024-02-11 14:05:15 +0530

    status improved

--- lf/.config/lf/lfcd.sh
+++ lf/.config/lf/lfcd.sh
@@ -18,32 +18,4 @@ lfcd () {
     # `command` is needed in case `lfcd` is aliased to `lf`
     cd "$(command lf -print-last-dir "$@")"
 }
-
-_zlf() {
-    emulate -L zsh
-    local d=$(mktemp -d) || return 1
-    {
-        mkfifo -m 600 $d/fifo || return 1
-        tmux split -bf zsh -c "exec {ZLE_FIFO}>$d/fifo; export ZLE_FIFO; exec lf" || return 1
-        local fd
-        exec {fd}<$d/fifo
-        zle -Fw $fd _zlf_handler
-    } always {
-        rm -rf $d
-    }
-}
-zle -N _zlf
-bindkey '\ek' _zlf
-
-_zlf_handler() {
-    emulate -L zsh
-    local line
-    if ! read -r line <&$1; then
-        zle -F $1
-        exec {1}<&-
-        return 1
-    fi
-    eval $line
-    zle -R
-}
-zle -N _zlf_handler
+bindkey -s '^o' 'lfcd\n'
